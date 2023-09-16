23a768f546868f99bc6764bf8217c857a58de562
ichristwin <ichristwin@gmail.com>
2023-09-16 17:46:16 +0100

    added access control

--- contracts/SBAT.sol
+++ contracts/SBAT.sol
@@ -2,18 +2,30 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "./IERC6551Registry.sol";
 
-contract SoulBoundAccountToken is ERC721 {
+contract SoulBoundAccountToken is ERC721, ERC721Burnable, AccessControl {
     error TranferProhibited();
-    error NotOwner();
+    bool transferAllowed = false;
+    bytes32 public constant TOGGLE_ROLE = keccak256("TOGGLE_ROLE");
+    bytes32 public constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("SoulBoundAccountToken", "SBTA") {}
+    constructor() ERC721("SoulBoundAccountToken", "SBTA") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(TRANSFER_ROLE, msg.sender);
+        _grantRole(TOGGLE_ROLE, msg.sender);
+    }
+
+    function toggleTransfers(bool state) external onlyRole(TOGGLE_ROLE) {
+        transferAllowed = state;
+    }
 
     function safeMint(address to) external {
         uint256 tokenId = _tokenIdCounter.current();
@@ -21,34 +33,33 @@ contract SoulBoundAccountToken is ERC721 {
         _safeMint(to, tokenId);
     }
 
-    function burn(uint256 tokenId) external {
-        if (msg.sender != ownerOf(tokenId)) revert NotOwner();
-        _burn(tokenId);
+    function tokenAccount(uint256 tokenId) public view returns (address) {
+        return
+            IERC6551Registry(0x02101dfB77FDE026414827Fdc604ddAF224F0921)
+                .account(
+                    0x2D25602551487C3f3354dD80D76D54383A243358,
+                    10,
+                    address(this),
+                    tokenId,
+                    0
+                );
     }
 
-    function tokenAccount(uint256 tokenId) public view returns (address) {
-        IERC6551Registry Registry = IERC6551Registry(
-            0x02101dfB77FDE026414827Fdc604ddAF224F0921
-        );
-        return
-            Registry.account(
-                0x2D25602551487C3f3354dD80D76D54383A243358,
-                10,
-                address(this),
-                tokenId,
-                0
-            );
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256
-    ) internal pure {
-        if (from != address(0) || to != address(0)) revert TranferProhibited();
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721) {
-        super._burn(tokenId);
+    ) internal view {
+        if (
+            !hasRole(TRANSFER_ROLE, from) ||
+            from != address(0) ||
+            to != address(0)
+        ) revert TranferProhibited();
     }
 }
